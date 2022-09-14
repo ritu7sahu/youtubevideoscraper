cebb8aecbe188deba9a8053f5dd8e6061aee2c92
ritu7sahu <ritu.sahu2014@gmail.com>
2022-09-14 19:38:26 +0530

    changed chromedriver path

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def index():
         # ("getting url from form after clicking on search button")
         url = request.form['content']
         videos_in_no = request.form['no_of_videos']
-        driver_path = r'E:\Python\project\youtubevideoscraper\chromedriver.exe'
+        driver_path = r'chromedriver.exe'
         driver = webdriver.Chrome(executable_path = driver_path)
         # ("creating driver and accessing url in Chrome")
         driver.get(url)
